e4db99bb2e02345e68b697b0743a2a3649e2b1d9
Vitaliy Kuzin <saynschuman@gmail.com>
2016-08-12 15:59:13 +0300

    2 commit

--- gruntfile.js
+++ gruntfile.js
@@ -12,18 +12,43 @@ grunt.initConfig({
       }
     },
     uglify: {
-    	dist: {
-    		src: ['js/script.main.js'],
-    		dest: 'js/script.main.min.js'
-    	}
-    }
+      dist: {
+        src: ['js/script.main.js'],
+        dest: 'js/script.main.min.js'
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'css/main.css' : 'sass/main.scss'
+        }
+      }
+    }, 
+    cssmin: {
+      my_target: {
+        files: [{
+          expand: true,
+          cwd: 'css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css/',
+          ext: '.min.css'
+        }]
+      }
+    },
+    watch: {
+      sass: {
+        files: ['sass/*.scss'],
+        tasks: ['sass', 'cssmin'],
+      }
+    }    
 });
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['concat', 'uglify']);	
+  grunt.registerTask('default', ['concat', 'uglify', 'sass', 'cssmin']);  
 };
 
